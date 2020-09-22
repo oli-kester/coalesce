29556d76a6888668124f6fd4f128c47c93246106
Oliver Reynolds <oli_kester@zoho.com>
2020-09-22 17:18:21 +0100

    Initial Commit. Installed eslint and fixed errors.

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Coalesce from './Coalesce';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
---
+++ src/Coalesce.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameCanvas from './gameCanvas';
+
+function Coalesce() {
+  return (
+    <div className="root-container">
+      <h1>Coalesce</h1>
+      <h2>Pure Reactive Joy.</h2>
+      <GameCanvas />
+    </div>
+  );
+}
+
+export default Coalesce;
