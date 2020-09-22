2741e90907262c4312e7143d93a3f25a3f026a72
Oliver Reynolds <oli_kester@zoho.com>
2020-09-22 20:30:59 +0100

    Now Drawing Player Sprite

--- src/gameCanvas.jsx
+++ src/gameCanvas.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PlayerSprite from './sprites/playerSprite';
 
 function GameCanvas() {
   return (
     <div className="canvas">
-        {/* <PlayerSprite /> */}
+      <PlayerSprite xPosStart="250" yPosStart="250" />
     </div>
   );
 }
--- src/sprites/playerSprite.jsx
+++ src/sprites/playerSprite.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function PlayerSprite() {
-  return (<div />);
+const PLAYER_SPRITE_STARTING_RADIUS = 12;
+
+// TODO breathing effects on player sprite
+
+function PlayerSprite(props) {
+  const { xPosStart, yPosStart } = props;
+
+  const [xPos, setXPos] = useState(xPosStart);
+  const [yPos, setYPos] = useState(yPosStart);
+  const [radius, setRadius] = useState(PLAYER_SPRITE_STARTING_RADIUS);
+
+  return (
+    <svg width="100%" height="100%">
+      <circle cx={xPos} cy={yPos} r={radius} fill="#44c5c5" />
+    </svg>
+  );
 }
+
+PlayerSprite.propTypes = {
+  xPosStart: PropTypes.number.isRequired,
+  yPosStart: PropTypes.number.isRequired,
+};
 
 export default PlayerSprite;
